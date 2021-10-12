25c9994871ce384c771da66396d46e968a8fc800
haoleqin <haoleqin@100tal.com>
2021-10-12 23:39:57 +0800

    pc适配

--- public/screeninit.js
+++ public/screeninit.js
@@ -1,7 +1,9 @@
 var screenInit = {			
   setDefaultFontSize: function() {					
       var fontsizeAll=window.innerWidth / 750 * 100;
-      				console.log(fontsizeAll);	
+      if(window.innerWidth > 750){
+        fontsizeAll = '100'
+      }
       document.querySelector('html').style.fontSize = fontsizeAll + 'px';
 
       // var bodyFontSize= parseFloat(window.getComputedStyle(document.getElementsByTagName('html')[0]).fontSize);													
